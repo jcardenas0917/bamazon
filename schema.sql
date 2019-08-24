c4e1f6f5d195243e36af01fa54766762a2af8f88
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-08-24 11:47:06 -0400

    modified stock column

--- schema.sql
+++ schema.sql
@@ -2,12 +2,12 @@ DROP DATABASE IF EXISTS bamazon;
 CREATE DATABASE bamazon;
 USE bamazon;
 
-CREATE TABLE products(
-  item_id INTEGER AUTO_INCREMENT NOT NULL,
-  product_name VARCHAR(50)NOT NULL,
-  department_name VARCHAR(50)NOT NULL,
-  price DECIMAL()NOT NULL,
-  stock_quantity INTEGER()NOT NULL,
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(50) NOT NULL,
+  department_name VARCHAR(50) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT NOT NULL,
   PRIMARY KEY (item_id)
 );
 
