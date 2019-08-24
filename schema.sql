dda80b0ea5713bbee375c8f94dd96f26620f7aa7
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-08-24 10:42:43 -0400

    change the table to show correctly

--- schema.sql
+++ schema.sql
@@ -4,10 +4,10 @@ USE bamazon;
 
 CREATE TABLE products(
   item_id INTEGER AUTO_INCREMENT NOT NULL,
-  product_name VARCHAR(50),
-  department_name VARCHAR(50),
-  price DECIMAL(),
-  stock_quantity INTEGER(),
+  product_name VARCHAR(50)NOT NULL,
+  department_name VARCHAR(50)NOT NULL,
+  price DECIMAL()NOT NULL,
+  stock_quantity INTEGER()NOT NULL,
   PRIMARY KEY (item_id)
 );
 
